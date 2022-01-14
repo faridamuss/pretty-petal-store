e3aa3896e143fd52ab36bad4942debdde2d638ab
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-14 18:59:58 -0500

    Create a product component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>
-            Copyright &copy; Shop
-          </Col>
+          <Col className="text-center py-3">Copyright &copy; Shop</Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
