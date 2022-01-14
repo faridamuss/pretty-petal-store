577007db0c979bfacf19d4cb7d910f9755c33ad4
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-14 16:17:32 -0500

    added header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'; 
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to Our Store</h1>
+  <Header/>
+   
+  <main>
+    <h1>Welcome to Our Store</h1>
+  </main>
+
+  <Footer/>
     </>
   );
 }
