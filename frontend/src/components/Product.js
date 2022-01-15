49eb2a2a3ad3f0c850fc1cff732c5c824f269624
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-15 14:26:37 -0500

    Import and update links to product

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,20 +1,21 @@
-import React from "react"
-import { Card } from "react-bootstrap"
-import Rating from './Rating'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
 
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as="div">
           {/* <div className="my-3">
@@ -22,14 +23,16 @@ const Product = ({ product }) => {
           </div>
            */}
 
-         <Rating value={product.rating} text={`${product.numReviews} reviews`} />  
-
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
 
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
