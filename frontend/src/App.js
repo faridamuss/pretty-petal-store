9c8931eb0e419478ca62fb570ae426f00a7594f3
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-15 14:12:37 -0500

    Import react router dom and create a homepage router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import React from 'react'; 
-import {Container} from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <>
-  <Header/>
-  <main className= 'py-3'>
-    <Container>
-      <HomeScreen/>
-    </Container>
-  </main>
-  <Footer/>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
