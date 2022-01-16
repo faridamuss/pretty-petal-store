7dedfa48fcaf04c56cb2c7a5c486d274415938db
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-16 14:30:41 -0500

    Bringing redux to the HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import { Row, Col } from 'react-bootstrap'
-import Product from '../components/Product'
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
-   
-    useEffect(() => {
-       const fetchProducts = async () => {
-           const { data } = await axios.get('api/products')
+  const dispatch = useDispatch();
 
-           setProducts(data)
-       }
-       fetchProducts()
-    }, [])
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (
-        <>
-           <h1>Latest Products</h1> 
-           <Row>
-               {products.map(product => (
-                   <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                   <Product product={product}/>
-                   </Col>
-               ))}
-           </Row>
-        </>
-    )
-}
+  const products = [];
 
-export default HomeScreen
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
