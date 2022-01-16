7bab68ba989a666e1953a36c440f3bb9a01188ce
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-16 15:38:15 -0500

    Message and Loader components are complete

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,7 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+    throw new Error("Some error");
     // res.status(401);
     // throw new Error("Not Authorized");
     res.json(products);
