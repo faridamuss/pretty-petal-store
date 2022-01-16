4be393ca8b53b4dc8a2cb5bbc7ef30e8ae589481
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-16 13:04:27 -0500

    fetching the data

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from 'colors'
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config();
 connectDB()
@@ -13,14 +15,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+
 
 const PORT = process.env.PORT || 5000;
 
