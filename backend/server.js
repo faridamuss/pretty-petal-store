26cb5b19fc5b1d5f730e33ed99e5bb0ffe5156a1
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-16 10:45:55 -0500

    started mongodb implementation

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from 'colors'
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+connectDB()
 
 const app = express();
 
@@ -22,5 +25,5 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
