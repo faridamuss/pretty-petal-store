a446e0bc85f88a8935ddcad3f5a859ac1f436dca
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-17 11:18:44 -0500

    Create cart reducer and add to cart action

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from "./config/db.js";
 
-
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config();
