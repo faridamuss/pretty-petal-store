116ab50b9426b6efe808138e1cd68bbb484e77b7
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-17 18:04:15 -0500

    custom authentication middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,5 +24,24 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password')
   }
 })
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
 
-export { authUser }
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+ })
+
+
+export { authUser, getUserProfile }
