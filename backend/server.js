1467b80fea11203d4c6f68444ff26cab7d4066aa
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-18 22:09:01 -0500

    fixed a typo

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
-import OrderRoutes from './routes/OrderRoutes.js'
+import orderRoutes from './routes/OrderRoutes.js'
 
 dotenv.config();
 connectDB()
