fb0e7441646665b593dda5dac1ac9088996573e7
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-18 22:11:16 -0500

    order-server connection

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
