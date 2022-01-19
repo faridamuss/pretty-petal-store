ef745ad05086b4cfb7e7cfdbf3baed1dd8eb2525
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-18 22:06:58 -0500

    added order routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import OrderRoutes from './routes/OrderRoutes.js'
 
 dotenv.config();
 connectDB()
