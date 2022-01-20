aa3eb57c4c1267ea98ec4964a825601eb825f777
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-20 12:04:40 -0500

    reset order state when logout

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,7 +1,7 @@
 import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_SUCCESS,
-  ORDER_CREATE_FAIL,ORDER_CREATE_RESET, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS,ORDER_PAY_FAIL,ORDER_PAY_RESET } from '../constants/orderConstants'
+  ORDER_CREATE_FAIL,ORDER_CREATE_RESET, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS,ORDER_PAY_FAIL,ORDER_PAY_RESET, ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_FAIL, ORDER_LIST_MY_SUCCESS, ORDER_LIST_MY_RESET} from '../constants/orderConstants'
 
 
   export const orderCreateReducer = (state = {}, action) => {
@@ -75,3 +75,26 @@ import {
         return state
     }
   }
+  export const orderListMyReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+      case ORDER_LIST_MY_REQUEST:
+        return {
+          loading: true,
+        }
+      case ORDER_LIST_MY_SUCCESS:
+        return {
+          loading: false,
+          orders: action.payload,
+        }
+      case ORDER_LIST_MY_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        }
+      case ORDER_LIST_MY_RESET:
+        return { orders: [] }
+      default:
+        return state
+    }
+  }
+  
