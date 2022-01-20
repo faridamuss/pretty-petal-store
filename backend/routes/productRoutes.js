5815497d244c404d2ee8193ae19090d3bd558a43
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-20 15:03:09 -0500

    Create a product create reducer

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,6 +9,7 @@ import {
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
+
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router
   .route("/:id")
