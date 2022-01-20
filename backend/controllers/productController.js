24fcc3f2aebdab108e66060f0736350a9398b71f
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-20 14:02:58 -0500

    Add route for creating a product from admin side

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,7 +24,6 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-
 // @desc    Delete a product
 // @route   DELETE /api/products/:id
 // @access  Private/Admin
@@ -38,6 +37,25 @@ const deleteProduct = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Product not found");
   }
+});
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  const createdProduct = await product.save();
+  res.status(201).json(product);
 });
 
 export { getProducts, getProductById, deleteProduct };
