35423a1eedc77ea8d2051899ed1e0c15e66196bf
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-20 14:20:04 -0500

    Add a functionality for updating a product from a admin side

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -55,7 +55,33 @@ const createProduct = asyncHandler(async (req, res) => {
     description: "Sample description",
   });
   const createdProduct = await product.save();
-  res.status(201).json(product);
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
 
 export { getProducts, getProductById, deleteProduct };
