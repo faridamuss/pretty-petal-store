e1ac9e482407b37a3fafff135457d555ce53feae
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-21 02:25:15 -0500

    Fixed order ID issue on OrderScreen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -65,7 +65,7 @@ const OrderScreen = () => {
       document.body.appendChild(script);
     };
 
-    if (!order || successPay || successDeliver) {
+    if (!order || successPay || successDeliver || order._id !== orderId) {
       dispatch({ type: ORDER_PAY_RESET });
       dispatch({ type: ORDER_DELIVER_RESET });
       dispatch(getOrderDetails(orderId));
