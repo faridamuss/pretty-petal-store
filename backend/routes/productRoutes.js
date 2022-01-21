990af78cb3bee14384b5e153a6b324120af845bb
Ekin Koluk <ekinkoluk@gmail.com>
2022-01-21 17:04:37 -0500

    css

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,13 +6,15 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
-  createProductReview
+  createProductReview,
+  getTopProducts
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
 router
   .route("/:id")
   .get(getProductById)
