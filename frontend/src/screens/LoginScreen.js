a4d3702424ebe968e5046c5db22c54aef9da172c
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-21 00:44:14 -0500

    Include navigate and useNavigate as a dependency

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Link,
-  useSearchParams,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
+import { Link, useSearchParams, useNavigate } from "react-router-dom";
 import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
@@ -17,30 +12,30 @@ const LoginScreen = () => {
   const [password, setPassword] = useState("");
   const [searchParams] = useSearchParams();
   const dispatch = useDispatch();
-  const userLogin = useSelector((state) => state.userLogin);
-  const { loading, error, userInfo } = userLogin;
+  const userLogin = useSelector(state => state.userLogin)
+  const {loading, error, userInfo} = userLogin;
   const navigate = useNavigate();
-  //const { search } = useLocation();
+  //const { search } = useLocation(); 
   //const { location} = useLocation();
   const redirect = [...searchParams].length > 0 ? [...searchParams][0][1] : "/";
   //const redirect = new URLSearchParams(search).get('querystringkey');
   //const redirect = location.search ? location.search.split('=')[1] : '/'
   useEffect(() => {
-    if (userInfo) {
-      navigate(redirect);
+    if(userInfo){
+      navigate(redirect)
     }
-  }, [userInfo, redirect]);
+  },[userInfo, navigate, redirect])
 
   const submitHandler = (e) => {
     e.preventDefault();
-    dispatch(login(email, password));
+    dispatch(login(email,password))
   };
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
-      {error && <Message variant="danger"> {error} </Message>}
-      {loading && <Loader />}
+      {error && <Message variant = 'danger'> {error} </Message>}
+      {loading && <Loader/>}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
