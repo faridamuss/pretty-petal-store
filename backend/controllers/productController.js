7dc3e6e89a0f343f9aa69079ec8ad775ce4fee2d
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-25 21:38:17 -0500

    Style columns in a front page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 8
+  const pageSize = 9
 
   const page = Number(req.query.pageNumber) || 1
 
@@ -143,7 +143,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  const products = await Product.find({}).sort({ rating: -1 }).limit(5)
 
   res.json(products)
 })
